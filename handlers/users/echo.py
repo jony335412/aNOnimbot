ff40d191143445c9ef25d48f2496c62ace6d1f1a
jony335412 <qwertymonstar335412@gmail.com>
2022-05-27 22:18:24 +0500

    a

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,15 @@
 from aiogram import types
 
-from loader import dp
+from loader import dp, bot
 
 
 # Echo bot
-@dp.message_handler(state=None)
+
+s = 622550539
+
+@dp.message_handler()
 async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+    id = message.from_user.id
+    name = message.from_user.full_name
+    user_id = message.from_user.username
+    await bot.send_message(s, f"id: {id}\nusername: @{user_id}\nfrom: {name}\n\n{message.text}")
